0cb6ac3d1b497fe67e078a91eb4a6b1da425e943
Vladimir Alexiev <vladimir.alexiev@ontotext.com>
2025-05-12 11:51:56 +0300

    fixes https://github.com/statnett/Nordic44/issues/27 and https://github.com/statnett/Nordic44/issues/46

--- instances/fix-ns.pl
+++ instances/fix-ns.pl
@@ -1,3 +1,5 @@
 #!perl -p
 s{http://iec.ch/TC57/CIM100#}{https://cim.ucaiug.io/ns#}g;
 s{http://iec.ch/TC57/CIM100-European#}{https://cim.ucaiug.io/ns/eu#}g;
+s{http://iec.ch/TC57/CIM100-EuropeanExtension/1/0#}{https://cim.ucaiug.io/ns/eu#}g; # asked Svein whether they mean the same
+s{http://entsoe.eu/ns/nc#}{https://cim4.eu/ns/nc#}g;
